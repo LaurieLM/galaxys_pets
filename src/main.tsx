75a5e4b7678ccae8b0fa2a565c16063b2af25e0a
Laurie <lespinasse.laurie5@gmail.com>
2026-02-19 02:57:01 +0100

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+      <h1>Hello, world!</h1>
+      <img src="src/assets/logo.png" alt="Logo" />
+  </StrictMode>,
+)
