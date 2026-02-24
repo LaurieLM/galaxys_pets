43bad74bd2baafb61bba646903b393707bb08f3d
Laurie <lespinasse.laurie5@gmail.com>
2026-02-24 19:31:07 +0100

    Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <footer className="bg-pink-50 text-center border-t border-gray-200 shadow-sm">
+            <p className="font-thin text-[0.80rem] pt-4 pb-4">
+                &copy; 2026 Galaxy's Pets - Plateforme d'informations animalières <br />
+                Tous droits réservés. <br /> <br />
+                Toutes les informations sont fournies à titre indicatif. <br />
+                Consultez toujours un vétérinaire professionnel.
+            </p>
+        </footer>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <div className='min-h-screen flex flex-col'>
       <Header />
+      <main className='flex-grow'>
+          {/* Contenu principal de l'application */}  
+      </main>
+      <Footer />
+    </div>
   </StrictMode>,
 )
