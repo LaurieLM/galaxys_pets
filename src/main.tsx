dd97dbff2da4e6d61840550fb6b2074bead74c01
Laurie <lespinasse.laurie5@gmail.com>
2026-02-27 07:45:56 +0100

    add navbar component

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className='min-h-screen flex flex-col'>
       <Header />
+      <Navbar />
       <main className='flex-grow'>
           {/* Contenu principal de l'application */}  
       </main>
