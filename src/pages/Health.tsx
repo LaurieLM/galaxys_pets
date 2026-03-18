d5c0fb7d43818e689968ff9ac29df6cd2aafac10
Laurie <lespinasse.laurie5@gmail.com>
2026-03-18 14:55:25 +0100

    update hook and display for health

--- src/pages/Health.tsx
+++ src/pages/Health.tsx
@@ -1,5 +1,19 @@
+import useHealth from "../hooks/useHealth";
+
 export default function Health() {
+    const animalId = 3;
+    const { data, isLoading, isError, error } = useHealth(animalId);
+
+    if (isLoading) return <p>Chargement...</p>;
+    if (isError) return <p>Erreur : {(error as Error).message}</p>;
+    if (!data) return <p>Aucune information sur la santé disponible.</p>;
     return (
-        <div>Santé</div>
+        <section>
+            <h2>Santé</h2>
+            <p>Type : {data.animalType}</p>
+            <p>Vermifuge : {data.dewormingFrequency}</p>
+            <p>Visite vétérinaire : {data.vetCheckFrequency}</p>
+            <p>Conseils : {data.generalAdvice}</p>
+        </section>
     )
 }
