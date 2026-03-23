cf800396cfa4e320a81f92cf4abf5582fd4f4983
Laurie <lespinasse.laurie5@gmail.com>
2026-03-23 14:09:00 +0100

    update for display data /type animal + add filter

--- src/pages/Health.tsx
+++ src/pages/Health.tsx
@@ -1,25 +1,69 @@
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import useHealth from "../hooks/useHealth";
+import getAnimals from "../api/animals";
 
 export default function Health() {
-    const animalId = 1;
-    const { data, isLoading, isError, error } = useHealth(animalId);
+    const [selectedAnimalId, setSelectedAnimalId] = useState<number>(0);
+    const { 
+        data: animals, 
+        isLoading: isLoadingAnimals, 
+        isError: isErrorAnimals, 
+        error: animalsError
+    } = useQuery({
+        queryKey: ['animals'],
+        queryFn: getAnimals,
+    });
 
-    if (isLoading) return <p>Chargement...</p>;
-    if (isError) return <p>Erreur : {(error as Error).message}</p>;
-    if (!data) return <p>Aucune information sur la santé disponible.</p>;
+    const {
+        data: health,
+        isLoading: isLoadingHealth,
+        isError: isErrorHealth,
+        error: healthError,
+    } = useHealth(selectedAnimalId);
+
+    if (isLoadingAnimals) return <p>Chargement des types d'animaux...</p>;
+    if (isErrorAnimals) return <p>Erreur types d'animaux: {(animalsError as Error).message}</p>;
+
+    
     return (
         <section>
                 <h2 className="text-center text-[#8fd3a9] font-bold text-3xl mt-4 mb-8">Santé</h2>
+
                 <div className="m-4">
-                    <h3 className="font-medium text-[1.1rem] text-[#b8e3c8]">Fréquence vermifuge</h3>
-                    <p className="text-center text-slate-300 font-thin mt-4 mb-8">{data.dewormingFrequency}</p>
-
-                    <h3 className="font-medium text-[1.1rem] text-[#b8e3c8]">Fréquence visite vétérinaire</h3>
-                    <p className="text-center text-slate-300 font-thin mt-4 mb-8">{data.vetCheckFrequency}</p>
-
-                    <h3 className="font-medium text-[1.1rem] text-[#b8e3c8]">Conseils</h3>
-                    <p className="text-justify text-slate-300 font-thin mt-4 mb-8 mr-4 ml-4">{data.generalAdvice}</p>
+                    <label>Type d'animal</label>
+                    <select value={selectedAnimalId}
+                    onChange={(e) => setSelectedAnimalId(Number(e.target.value))}>
+                        <option value={0}>Sélectionnez un type d'animal</option>
+                        {animals?.map((animal) => (
+                            <option key={animal.id} value={animal.id}>
+                                {animal.type}
+                            </option>
+                        ))}
+                    </select>
                 </div>
+
+                {selectedAnimalId === 0 && (
+                    <p className="text-center text-slate-300 font-thin mt-4 mb-8">Veuillez sélectionner un type d'animal pour voir les recommandations de santé.</p>
+                )}
+
+                {selectedAnimalId > 0 && isLoadingHealth && <p>Chargement des recommandations de santé...</p>}
+                {selectedAnimalId > 0 && isErrorHealth && (
+                    <p>Erreur : {(healthError as Error).message}</p>
+                )}
+
+                {selectedAnimalId > 0 && health && (
+                    <div className="m-4">
+                        <h3 className="font-medium text-[1.1rem] text-[#b8e3c8]">Fréquence vermifuge</h3>
+                        <p className="text-center text-slate-300 font-thin mt-4 mb-8">{health.dewormingFrequency}</p>
+
+                        <h3 className="font-medium text-[1.1rem] text-[#b8e3c8]">Fréquence visite vétérinaire</h3>
+                        <p className="text-center text-slate-300 font-thin mt-4 mb-8">{health.vetCheckFrequency}</p>
+
+                        <h3 className="font-medium text-[1.1rem] text-[#b8e3c8]">Conseils</h3>
+                        <p className="text-justify text-slate-300 font-thin mt-4 mb-8 mr-4 ml-4">{health.generalAdvice}</p>
+                    </div>
+                )}
             </section>
-    )
-}
+        );
+    }
