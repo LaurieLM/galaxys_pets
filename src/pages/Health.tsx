0a5627c1442641389b2ad058d5760a8eb91c1475
Laurie <lespinasse.laurie5@gmail.com>
2026-03-24 00:51:45 +0100

    add display disease data + style

--- src/pages/Health.tsx
+++ src/pages/Health.tsx
@@ -3,6 +3,8 @@ import { useQuery } from "@tanstack/react-query";
 import useHealth from "../hooks/useHealth";
 import getAnimals from "../api/animals";
 import useVaccines from "../hooks/useVaccines";
+import useDiseases from "../hooks/useDiseases";
+import diseases from "../api/diseases";
 
 export default function Health() {
 
@@ -34,6 +36,13 @@ export default function Health() {
         isError: isErrorVaccines, 
         error: vaccinesError
     } = useVaccines(selectedAnimalId);
+
+    const {
+        data: diseases,
+        isLoading: isLoadingDiseases,
+        isError: isErrorDiseases,
+        error: diseasesError
+    } = useDiseases(selectedAnimalId);
 
     // Ferme le dropdown si on clique en dehors
     useEffect(() => {
@@ -131,7 +140,7 @@ export default function Health() {
                 )}
 
                 {/* Affichage des vaccins recommandés */}
-                {selectedAnimalId > 0 && <h3 className="m-4 font-medium text-[1.1rem] text-[#b8e3c8]">Vaccins recommandés</h3>}
+                {selectedAnimalId > 0 && <h3 className="m-4 font-black text-[1.3rem] text-[#b8e3c8] text-center">Vaccins recommandés</h3>}
 
                 {selectedAnimalId > 0 && isLoadingVaccines && (
                     <p className="text-center text-slate-300 font-thin mt-4 mb-8">Chargement des vaccins...</p>
@@ -160,6 +169,37 @@ export default function Health() {
 
                                     <h4 className="font-[300] text-[1rem] text-[#b8e3c8]">Rappels</h4>
                                     <li className="text-slate-300 font-thin text-center mt-4">{vaccine.reminders}</li>
+                                </ul>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+
+                {/* Affichage des maladies courantes */}
+                {selectedAnimalId > 0 && <h3 className="m-4 font-black text-[1.3rem] text-[#b8e3c8] text-center">Maladies courantes</h3>}
+
+                {selectedAnimalId > 0 && isLoadingDiseases && (
+                    <p className="text-center text-slate-300 font-thin mt-4 mb-8">Chargement des maladies...</p>
+                )}
+
+                {selectedAnimalId > 0 && isErrorDiseases && (
+                    <p className="text-center text-red-400 mt-4 mb-8">Erreur maladies : {(diseasesError as Error).message}</p>
+                )}
+
+                {selectedAnimalId > 0 && diseases && diseases.length === 0 && (
+                    <p className="text-center text-slate-300 font-thin mt-4 mb-8">Aucune maladie trouvée pour cet animal.</p>
+                )}
+
+                {selectedAnimalId > 0 && diseases && diseases.length > 0 && (
+                    <ul className="ml-4 mr-4 space-y-4">
+                        {diseases.map((disease) => (
+                            <li key={disease.name} className="bg-slate-800/40 rounded p-4">
+                                <ul>
+                                    <h3 className="inline-block py-1 px-4 text-[#b8e3c8] text-[1.1rem] font-[800] bg-[#f5787838] text-center rounded-[0.9rem]">{disease.name}</h3>
+
+                                    <h4 className="font-[300] text-[1rem] text-[#b8e3c8] mt-4">Description</h4>
+                                    <li className="text-slate-300 font-thin text-justify mt-4 mb-4">{disease.description}</li> 
+
                                 </ul>
                             </li>
                         ))}
