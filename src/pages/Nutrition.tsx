d236ef18055e1062eac46c3a27e4305400b6d774
Laurie <lespinasse.laurie5@gmail.com>
2026-03-24 01:29:13 +0100

    add title page

--- src/pages/Nutrition.tsx
+++ src/pages/Nutrition.tsx
@@ -1,5 +1,9 @@
 export default function Nutrition() {
     return (
-        <div>Nutrition</div>
+        <section>
+            <h2 className="mt-4 mb-8 flex items-center justify-center gap-3 text-center text-3xl font-bold text-[#d89262]">
+                Nutrition
+            </h2>
+        </section>
     )
 }
