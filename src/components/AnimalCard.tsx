5ba23cf732bd73af46b9c98e2f700942d25788ca
Laurie <lespinasse.laurie5@gmail.com>
2026-03-30 13:01:16 +0200

    fix key forget

--- src/components/AnimalCard.tsx
+++ src/components/AnimalCard.tsx
@@ -23,7 +23,7 @@ export default function AnimalCard() {
             {animals && animals.length > 0 && (
                 <ul className="ml-4 mr-4 space-y-4">
                     {animals.map((animal) => (
-                        <li className="rounded bg-slate-800/40 p-4">
+                        <li key={animal.id} className="rounded bg-slate-800/40 p-4">
                             <h3 className="inline-block rounded-[0.9rem] px-4 py-1 text-center text-[1.1rem] font-[800]">{animal.type}</h3>
                         </li>
                     ))}
