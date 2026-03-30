448411538765c6d7fe5d369685088988f29d6294
Laurie <lespinasse.laurie5@gmail.com>
2026-03-30 16:44:49 +0200

    update footer for margin in health, nutrition, shelter page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,10 @@
-export default function Footer() {
+type FooterProps = {
+    className?: string;
+};
+
+export default function Footer({ className = "" }: FooterProps) {
     return (
-        <footer className="bg-slate-800 text-white text-center">
+        <footer className={`bg-slate-800 text-white text-center ${className}`}>
             <p className="font-thin text-[0.80rem] pt-4 pb-4">
                 &copy; 2026 Galaxy's Pets - Plateforme d'informations animalières <br />
                 Tous droits réservés. <br /> <br />
