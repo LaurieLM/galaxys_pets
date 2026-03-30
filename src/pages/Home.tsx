a717a153daee9b4a6f558833c2a77a0c45b482aa
Laurie <lespinasse.laurie5@gmail.com>
2026-03-30 12:03:30 +0200

    add choose animal message

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,10 @@ export default function Home() {
                 <h1 className="text-[2.5rem] text-center text-[#8fd3a9] font-bold">Bienvenue sur Galaxy's Pets !</h1>
                 <p className="mt-4 text-[1.1rem] text-center text-slate-300 font-thin ">Découvre tout ce qu'il faut savoir pour prendre soin de ton animal de compagnie.</p>
             </div>
+            <div>
+                <h2 className="text-[1.5rem] text-center text-[#8fd3a9] font-bold mt-8">Choisis un type d'animal pour commencer :</h2>
+
+            </div>
         </section>
         
     )
