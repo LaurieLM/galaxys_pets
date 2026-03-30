d6d7d2333610133a1d0d73b4e8985b4b42d19aea
Laurie <lespinasse.laurie5@gmail.com>
2026-03-30 16:16:42 +0200

    delete dropdown + filter

--- src/pages/Health.tsx
+++ src/pages/Health.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import healthSectionLogo from "../assets/health_section_logo.svg";
-import Dropdown from "../components/Dropdown";
-import Filter from "../components/Filter";
 import GeneralInfo from "../components/GeneralInfo";
 import VaccineInfo from "../components/VaccineInfo";
 import Disease from "../components/Disease";
@@ -24,40 +22,10 @@ export default function Health() {
 
     return (
         <section> 
-            <Navbar />
-            {/* Dropdown pour sélectionner le type d'animal */}
-            <Dropdown selectedAnimalId={selectedAnimalId} setSelectedAnimalId={setSelectedAnimalId} />
-
             <h2 className="flex items-center justify-center gap-3 text-center text-3xl font-bold text-[#8fd3a9] mb-4">
                 <img src={healthSectionLogo} alt="Icône santé" className="h-12 w-12" />
                 Santé
             </h2>
-
-            {/* Filtres pour les vaccins et les maladies */}
-            <div className="flex justify-around">
-                <Filter
-                    label="Filtrer par vaccins"
-                    options={[
-                        { value: "vaccine", label: "Vaccins" },
-                    ]}
-                    selectedValue="vaccine"
-                    onChange={(value) => console.log(value)}
-                />
-
-                <Filter
-                    label="Filtrer par maladies"
-                    options={[
-                        { value: "disease", label: "Maladies" },
-                    ]}
-                    selectedValue="disease"
-                    onChange={(value) => console.log(value)}
-                />
-            </div>
-
-
-            {selectedAnimalId === 0 && (
-                <p className="text-[1.2rem] text-center text-slate-300 font-thin mt-32 mr-4 ml-4">Veuillez sélectionner un type d'animal pour voir les recommandations de santé.</p>
-            )}
 
             {/* Affichage des recommandations de santé pour l'animal sélectionné */}
             <GeneralInfo selectedAnimalId={selectedAnimalId} />
@@ -67,6 +35,8 @@ export default function Health() {
 
             {/* Affichage des maladies courantes */}
             <Disease selectedAnimalId={selectedAnimalId} />
+            
+            <Navbar />
         </section>
     );
 }
