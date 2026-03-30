257cac9c3720b39cda9f828e64cb97769538e547
Laurie <lespinasse.laurie5@gmail.com>
2026-03-30 11:40:15 +0200

    update route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Shelters from "./pages/Shelters";
 import Nutrition from "./pages/Nutrition";
 import Health from "./pages/Health";
+import Home from "./pages/Home";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -14,7 +15,8 @@ export default function App() {
             
             <main className="flex-grow">
                 <Routes>
-                    <Route path="/home" element={<Navigate to="/home" replace />} />
+                    <Route path="/" element={<Navigate to="/home" replace />} />
+                    <Route path="/home" element={<Home />} />
                     <Route path="/shelters" element={<Shelters />} />
                     <Route path="/health" element={<Health />} />
                     <Route path="/nutrition" element={<Nutrition />} />
