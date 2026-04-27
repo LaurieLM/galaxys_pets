a06d9442e12ca6b66a3d55b990e8cfeaecc29012
Laurie <lespinasse.laurie5@gmail.com>
2026-04-27 08:42:10 +0200

    update shelter card

--- src/components/ShelterCard.tsx
+++ src/components/ShelterCard.tsx
@@ -36,15 +36,19 @@ export default function ShelterCard({ selectedCity }: ShelterCardProps) {
             {filteredShelters && filteredShelters.length > 0 && (
                 <ul className="flex flex-wrap justify-center items-center mx-4 my-8">
                     {filteredShelters.map((shelter) => (
-                        <li key={shelter.id} className="flex flex-col items-center m-2 p-4 border rounded-lg shadow-md">
-                            <h3 className="text-[1.4rem] font-semibold">{shelter.name}</h3>
-                            <img src={shelter.image_url} alt={shelter.name} className="w-28 h-28 rounded" />
-                            <p className="text-sm text-gray-600">{shelter.city}</p>
-                            <p className="text-sm text-gray-600">{shelter.adress}</p>
-                            <p className="text-sm text-gray-600">{shelter.phone}</p>
-                            <a href={shelter.website} className="text-sm text-blue-500" target="_blank" rel="noopener noreferrer">{shelter.website}</a>
-                            <p className="text-sm text-gray-600 text-justify">{shelter.description}</p>
+                        <li key={shelter.id} className="flex flex-col items-center m-4 p-4 bg-slate-700 border rounded-lg shadow-md">
+                            <h3 className="mb-4 text-[1.4rem] font-semibold">{shelter.name}</h3>
+                            <img src={shelter.image_url} alt={shelter.name} className="w-60 h-36 rounded" />
+                            <p className="m-4 text-[0.9rem]  text-slate-200 text-justify">{shelter.description}</p>
+                            <div className="w-[100%] p-4 flex flex-col items-center border bg-slate-800 rounded-lg">
+                                {shelter.adress && <p className="mt-4 text-sm text-slate-200">{shelter.adress}</p>}
+                                <p className="text-sm text-slate-200">{shelter.city}</p>
+                                {shelter.phone && <p className="mt-4 text-sm text-slate-200">{shelter.phone}</p>}
+                                {shelter.website && <a href={shelter.website} className="mt-4 text-sm text-blue-500" target="_blank" rel="noopener noreferrer">Voir le site</a>}
+                            </div>
                         </li>
+                            
+                            
                     ))}
                 </ul>
             )}
